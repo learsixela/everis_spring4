27d0b70a70fd2679fce937dbc4dffac329e74ce4
Israel Alexis Palma Quezada <ipalm001@gmail.com>
2021-05-04 11:56:14 -0400

    Lazy

--- src/main/java/com/everis/data/controllers/LicenciaController.java
+++ src/main/java/com/everis/data/controllers/LicenciaController.java
@@ -1,5 +1,7 @@
 package com.everis.data.controllers;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 //import java.util.List;
@@ -30,17 +32,20 @@ public class LicenciaController {
 		//obtener todas las personas
 		//List<Persona> listaPersonas = pService.findAll();
 		model.addAttribute("listaPersonas", pService.findAll());
+		//consultar objeto por Lazy
+		//Licencia licencia = lService.finById(1L);
 		return "licencia.jsp";
 	}
 	
 	@RequestMapping("/insertar")
 	public String insertarLicencia(@RequestParam("numero") String numero,
 	@RequestParam("estado") String estado,
-	@RequestParam("fecha") Date fecha,
+	@RequestParam("expiracion") String expiracion,
 	@RequestParam("persona") Persona persona) {
 
 		boolean state;
 		Licencia licencia = new Licencia();
+		
 		licencia.setNumero(numero);
 		if(estado.equals("1")) {
 			state=true;
@@ -48,13 +53,19 @@ public class LicenciaController {
 			state=false;
 		}
 		licencia.setEstado(state);
-		licencia.setExpiracion(fecha);
+		//fechas de String a Date
+		try {
+			Date fecha=new SimpleDateFormat("yyyy-MM-dd").parse(expiracion);
+			licencia.setExpiracion(fecha);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			System.out.println(""+e);
+		}
+		
 		//Agregar persona
 		licencia.setPersona(persona);
 
-		//licencia= licenciaService.save(licencia);
-		
-		
+		lService.save(licencia);
 		
 		return "redirect:/licencia";
 	}
--- src/main/java/com/everis/data/services/LicenciaService.java
+++ src/main/java/com/everis/data/services/LicenciaService.java
@@ -3,6 +3,7 @@ package com.everis.data.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.everis.data.models.Licencia;
 import com.everis.data.repositories.LicenciaRepository;
 
 @Service
@@ -10,4 +11,12 @@ public class LicenciaService {
 
 	@Autowired
 	LicenciaRepository lRepository;
+
+	public Licencia save(Licencia licencia) {
+		return lRepository.save(licencia);
+	}
+
+	public Licencia finById(long id) {
+		return lRepository.findById(id).get();
+	}
 }
