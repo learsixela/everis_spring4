6100cc1dae92a4176ffc0cc84d02bbc802a96e96
Israel Alexis Palma Quezada <ipalm001@gmail.com>
2021-05-06 13:25:17 -0400

    spring Security configuracion

--- src/main/java/com/everis/data/auth/WebSecurityConfig.java
+++ src/main/java/com/everis/data/auth/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.everis.data.auth;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,8 +14,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.
             authorizeRequests()
-                .antMatchers("/static/**", "/registro","/persona/**").permitAll()
-                .anyRequest().authenticated()
+                .antMatchers("/css/**", "/js/**", "/registro","/persona/**").hasRole("ROL_ADMIN")
+                .antMatchers("/producto/**","/persona/**").hasAnyRole("ROL_ADMIN","ROL_USER")
+                .antMatchers("/").permitAll()
+                .anyRequest()
+                .authenticated()
                 .and()
             .formLogin()
                 .loginPage("/login")
@@ -23,4 +27,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .logout()
                 .permitAll();
     }
+    
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1").password("123").roles("ROL_ADMIN")
+                .and()
+                .withUser("user2").password("123").roles("ROL_USER");
+    }
+    
+    
 }
